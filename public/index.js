6cd9299e247590337f2833af4ab562f58b7e90fc
Sahil Arora <Arora@Sahils-MacBook-Pro-2.local>
2016-01-16 13:08:36 -0800

    deleted index.css. Added blank to category dropdown, Added firebase functionality, Added delete task button. Wrapped in bootstrap. git push

--- public/index.js
+++ public/index.js
@@ -1,28 +1,33 @@
 $(document).ready(function() {
 	var myDataRef = new Firebase('https://getshitdonenow.firebaseIO.com');
-    myDataRef.on('child_added', function(snapshot) {
-    	var task = snapshot.val();
-    	console.log(task);
-
-    	var category_column = "<td>"+task.category+"</td>";
-    	var owner_column = "<td>"+task.owner+"</td>";
-    	var task_column = "<td>"+task.task+"</td>";
-
-    	var new_row = "<tr>"+category_column+owner_column+task_column+"</tr>";
-    	$("#table").append(new_row);
-    	console.log(new_row);
-
-});
-    $("#button").click(function(){
-        var task = $("#new_task").val();
-        var category = $("#category option:selected").text();
-        var owner = $("#owner option:selected").text();
-        myDataRef.push(
-        	{
-        		task: task,
-        		category: category,
-        		owner: owner
-        	}
-        );
-    }); 
+	myDataRef.on('child_added', function(snapshot) {
+		var task = snapshot.val();
+		console.log(task);
+		var new_row = $("<tr></tr>");
+		var category_column = $("<td>"+task.category+"</td>");
+		var owner_column = $("<td>"+task.owner+"</td>");
+		var task_column = $("<td>"+task.task+"</td>");
+		var button_column = $("<td></td>");
+		var button = $('<button>-</button>').click(function () {
+			myDataRef.child(snapshot.key()).remove();
+		});
+		button_column.append(button);
+		new_row.append(task_column).append(category_column).append(owner_column).append(button_column);
+		$("#table").append(new_row);
+		myDataRef.child(snapshot.key()).on("child_removed", function(snapshot){
+			new_row.remove();
+		});
+	});
+	$("#button").click(function(){
+		var task = $("#new_task").val();
+		var category = $("#category option:selected").text();
+		var owner = $("#owner option:selected").text();
+		myDataRef.push(
+		{
+			task: task,
+			category: category,
+			owner: owner
+		}
+		);
+	}); 
 });
