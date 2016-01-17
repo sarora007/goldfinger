5b4063c6dc5a1a76a614000a9a2ff9c41d8003b3
Sahil Arora <Arora@Sahils-MacBook-Pro-2.local>
2016-01-16 16:49:14 -0800

    added due date column and functionality

--- public/index.js
+++ public/index.js
@@ -1,33 +1,63 @@
 $(document).ready(function() {
+
 	var myDataRef = new Firebase('https://getshitdonenow.firebaseIO.com');
+
+	$("#button").click(function(){
+		addTask();
+	});
+
+	$(document).keypress(function(e) {
+    	if(e.which == 13) {
+	    	addTask();
+    	}
+	});
+	$( "#datepicker" ).datepicker({
+			changeMonth: true,
+			changeYear: true
+		});
 	myDataRef.on('child_added', function(snapshot) {
 		var task = snapshot.val();
-		console.log(task);
+
 		var new_row = $("<tr></tr>");
+
 		var category_column = $("<td>"+task.category+"</td>");
 		var owner_column = $("<td>"+task.owner+"</td>");
 		var task_column = $("<td>"+task.task+"</td>");
+		var due_column =$("<td>"+task.due+"<td>");
 		var button_column = $("<td></td>");
 		var button = $('<button>-</button>').click(function () {
 			myDataRef.child(snapshot.key()).remove();
 		});
+
 		button_column.append(button);
-		new_row.append(task_column).append(category_column).append(owner_column).append(button_column);
+		new_row.append(task_column).append(category_column).append(owner_column).append(due_column).append(button_column);
 		$("#table").append(new_row);
+
 		myDataRef.child(snapshot.key()).on("child_removed", function(snapshot){
 			new_row.remove();
 		});
 	});
-	$("#button").click(function(){
+
+	var addTask = function(){
 		var task = $("#new_task").val();
 		var category = $("#category option:selected").text();
 		var owner = $("#owner option:selected").text();
-		myDataRef.push(
-		{
-			task: task,
-			category: category,
-			owner: owner
+		var due = $("#datepicker").val();
+		console.log(due);
+		if(category==""||owner =="" || task==""||due==""){
+			alert("Fill in the task details and keep our data clean, NIGGA");
 		}
-		);
-	}); 
+		else {
+			myDataRef.push({
+				task: task,
+				category: category,
+				owner: owner,
+				due: due
+			});
+			$("#new_task").val("");
+			$("#category").val("");
+			$("#owner").val("");
+			$('#datepicker').datepicker('setDate', null);
+		};
+	};
 });
